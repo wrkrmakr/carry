9b24244397f5436310d9f9139df4347e59014cf3
Wesley Hamilton <wesleychamilton@gmail.com>
2013-08-08 12:22:15 -0400

    renamed a file and small changes

---
+++ aboutUsLibrary.php
@@ -0,0 +1,37 @@
+<?php function aboutUSContent() { ?>
+        <div id="pageContent">
+          <div id="aboutUsImage">             
+            </div>
+            <div id="aboutCarry">
+              <span id="aboutCarryTitle">ABOUT CARRY MATERNITY</span><br><br>
+              <p>
+                Rhonda Maternity, a historic maternity boutique in the heart of Yorkville has transformed into Carry                      Maternity.At Carry Maternity, we provide the same attentive care and impeccable products that have been                       bringing pregnant mothers into Rhonda Maternity since 1971.
+              </p>
+            </div><br><br>
+            <div id="featuresSet1">
+               <div id="feature1" class="feature">
+                  <span>Nourishing Space</span><br><br>
+                    <p>Carry Maternity offers new and expecting mothers a nourishing space where they can pamper themselves. Our                               goal is to provide exceptional care to new mothers, so they can better care for themselves and their                       babies.
+                  </p>  
+                </div>  
+                <div id="feature2" class="feature">
+                  <span>Affordable Fashion</span><br><br>
+                    <p>At Carry Maternity we know concerns that come with having your first baby. We will help you to find                         something on a budget. Our mantra is spend well, spend less..
+                  </p>
+                </div>  
+            </div>
+            
+            <div id="featuresSet2">
+               <div id="feature3" class="feature">
+                  <span>Pregnancy in Style</span><br><br>
+                    <p>Carry Maternity offers a wide-array of original and brand name designs and an exclusive selection of                        all-natural cosmetics products.
+                  </p>  
+                </div>  
+                <div id="feature4" class="feature">
+                  <span>Here to Help</span><br><br>
+                    <p>We strive to be a supportive space for new and expecting mothers in Toronto. If you are in the                                 neighbourhood, stop by for one of our workshops, or come in to change a diaper or nurse your child.
+                  </p>
+                </div>  
+            </div>
+        </div>
+<?php } ?>

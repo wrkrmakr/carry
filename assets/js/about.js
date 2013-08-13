a92ae4ce591206cb985f0aff5d3cf08fa3de5be0
Wesley Hamilton <wesleychamilton@gmail.com>
2013-08-13 11:03:53 -0400

    made the bar animate slower and thinned the bar (smaller height)

--- assets/js/about.js
+++ assets/js/about.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
-   var progressPercent;
+   var progressPercent = 0;
    var imageIndex = 1;
    var imagesInBanner = 5;
+   var pVal = $('.ui-progressbar-value').addClass('ui-corner-right');
    var myImages = [
       "assets/img/about/aboutBanner1.jpg",
       "assets/img/about/aboutBanner2.jpg",
@@ -15,6 +16,7 @@ $(document).ready(function() {
    $(window).load(function(){
       $("#progressbar").progressbar();
       progressBar();
+      
    });
 
    $('#aboutUsImage, .progressbar').click(function(e) {
@@ -37,26 +39,30 @@ $(document).ready(function() {
    }
 
    function progressBar(){
-      progressPercent = 0;
       var timer = setInterval (function () {
-         $('#progressbar').progressbar ("value", progressPercent);
-         progressPercent++;
+         $('#progressbar div').stop(true).animate({width: progressPercent + '%'}, 186, function(){
+            progressPercent++;
+         });
+         $('#progressbar div').css('display','block');
+         //$('#progressbar').progressbar ("value", progressPercent);
+         
          if (progressPercent > 100){
+            progressPercent = -2;
             toNextPhoto();
-            progressPercent = -5;
          }
-      }, 50);
+      }, 200);
    }
 
    function toNextPhoto() {
       if (imageIndex == imagesInBanner)
          imageIndex = 1;
       imageIndex = parseInt(imageIndex)+1;
-
+      $('progressbar').hide(0);
       $('#aboutUsImageCrossFade').css('background-image','url(assets/img/about/aboutBanner'+imageIndex+'.jpg)');
-      $('#aboutUsImageCrossFade').show("fade", 400);
-      $('#aboutUsImage').hide("fade", 400, function() {
+      $('#aboutUsImageCrossFade').show("fade", 500);
+      $('#aboutUsImage').hide("fade", 500, function() {
          $('#aboutUsImage').css('background-image','url(assets/img/about/aboutBanner'+imageIndex+'.jpg)');
+         $('progressbar').show(0);
          $('#aboutUsImage').show(0);
          $('#aboutUsImageCrossFade').hide(0);
       });

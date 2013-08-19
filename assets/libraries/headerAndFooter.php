bd61b9fcb03a9ec6c85ed04caeeb27383a7d3e59
Talwinder Singh <nijjar.login@gmail.com>
2013-08-19 12:23:13 -0400

    added nav and logo

--- assets/libraries/headerAndFooter.php
+++ assets/libraries/headerAndFooter.php
@@ -62,108 +62,18 @@
     	<div id="logoCont">
         	<img src="assets/img/official_carry_logo_2013.png" alt="Carry Maternity Logo" title="Carry Maternity">
         </div>	
-        <div id="navCont"></div>
+        <div id="navCont">
+        	<ul id="nav">
+            	<li>NEW ARRIVALS</li><br />
+                <li>DEPARTMENTS</li><br />
+                <li>DESIGNERS</li><br />
+                <li>LOOKBOOK</li><br />
+                <li>CONTACT US</li><br />
+                <li>BLOG</li><br />
+            </ul>
+        </div>
     </div>
-        <!--<div id="header">
-                <div id ="logoImg"><a href="index.php"><img src="assets/img/CM_Logo.png" border="0" /></a></div>
-            <div id="nav">
-                <ul id="navLeft">
-                    <li id ="department" style="margin-left:2px" class="subMenuLink">
-                       <span>DEPARTMENTS</span>
-                        <div id="departmentSubMenu" class="subMenuList">
-                            <table>
-                                <tr>
-                                    <td colspan="3">VIEW ALL</td>
-                                    <td class="subMenuItem" colspan="3">Dresses</td>
-                                </tr>
-                                <tr>
-                                    <td colspan="3"></td>
-                                    <td class="subMenuItem" colspan="3">Denim</td>
-                                </tr>
-                                <tr>
-                                    <td colspan="3"></td>
-                                    <td class="subMenuItem" colspan="3">Tops</td>
-                                </tr>
-                                <tr>
-                                    <td colspan="3"></td>
-                                    <td class="subMenuItem" colspan="3">Skirts</td>
-                                </tr>
-                                <tr>
-                                    <td colspan="3"></td>
-                                    <td class="subMenuItem" colspan="3">Nursing</td>
-                                </tr>
-                                <tr>
-                                    <td colspan="3"></td>
-                                    <td class="subMenuItem" colspan="3">Coats &amp; Jackets</td>
-                                </tr>
-                                <tr>
-                                    <td colspan="3"></td>
-                                    <td class="subMenuItem" colspan="3">Maternity</td>
-                                </tr>
-                                <tr>
-                                    <td colspan="3"></td>
-                                    <td class="subMenuItem" colspan="3">Accessories</td>
-                                </tr>
-                            </table>                            
-                        </div>                    
-                    </li>
-                    <a href="#"><li class="liPaddingLeft" class="menuLink">LOOK BOOK</li></a>
-                    <li id ="designer" class="liPaddingLeft subMenuLink">
-                        <span>DESIGNERS</span>
-                        <div id="designerSubMenu" class="subMenuList">
-                            <table>
-                                <tr>
-                                    <td colspan="3">VIEW ALL</td>
-                                    <td class="subMenuItem" colspan="3">Carry Maternity</td>
-                                    <td class="subMenuItem" colspan="3">Paige</td>
-                                    <td class="subMenuItem" colspan="3">Queen Mum</td>
-                                </tr>
-                                <tr>
-                                    <td colspan="3"></td>
-                                    <td class="subMenuItem" colspan="3">Olian Maternity</td>
-                                    <td class="subMenuItem" colspan="3">BellaBand</td>
-                                    <td class="subMenuItem" colspan="3">Neka</td>
-                                </tr>
-                                <tr>
-                                    <td colspan="3"></td>
-                                    <td class="subMenuItem" colspan="3">Maichael Stars</td>
-                                    <td class="subMenuItem" colspan="3">Isabella Oliver</td>
-                                    <td class="subMenuItem" colspan="3">boob</td>
-                                </tr>
-                                <tr>
-                                    <td colspan="3"></td>
-                                    <td class="subMenuItem" colspan="3">OiOi</td>
-                                    <td class="subMenuItem" colspan="3">Citizens of Humanity</td>
-                                    <td class="subMenuItem" colspan="3">SoYoung</td>
-                                </tr>
-                                <tr>
-                                    <td colspan="3"></td>
-                                    <td class="subMenuItem" colspan="3">Noppies</td>
-                                    <td class="subMenuItem" colspan="3">Ripe Maternity</td>
-                                    <td class="subMenuItem" colspan="3">The M Coat</td>
-                                </tr>
-                                <tr>
-                                    <td colspan="3"></td>
-                                    <td class="subMenuItem" colspan="3">Cake Lingerie</td>
-                                    <td class="subMenuItem" colspan="3">Jules &amp; Jimi</td>
-                                    <td class="subMenuItem" colspan="3">bravado!</td>
-                                </tr>
-                                <tr>
-                                    <td colspan="3"></td>
-                                    <td class="subMenuItem" colspan="3">Petunia Pickle Bottom</td>
-                                    <td class="subMenuItem" colspan="3">Belabumbum</td>
-                                </tr>
-                            </table>                              
-                        </div>
-                    </li>
-                </ul>
-                <ul id="navRight">
-                    <a href="#"><li class="liPaddingRight menuLink">BLOG</li></a>
-                    <a href="about.php"><li class="liPaddingRight menuLink">ABOUT US</li></a>
-                    <a href="contact.php"><li style="margin-right:2px " class="menuLink">CONTACT</li></a>
-                </ul>
-            </div>
-        </div>-->
+       
 
 <?php } ?>
 <?php function carryHTMLFooter() { ?>

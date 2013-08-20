f28aaf6a60eac6765bb5c8ff9e9acad2b4bd32cf
Talwinder Singh <nijjar.login@gmail.com>
2013-08-20 16:17:05 -0400

    added department page

---
+++ departments.php
@@ -0,0 +1,9 @@
+<?php
+   include "assets/libraries/headerAndFooter.php";
+   include "assets/libraries/departmentsLibrary.php";
+   
+   carryHTMLHead("departments","departments"," - Departments");
+   carryNavigationPanel();
+   departmentsContent();
+   carryHTMLFooter();
+?>

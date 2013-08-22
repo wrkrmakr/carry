197f859627bcf88ef4341b8fbbf4afd771d5c9f0
Talwinder Singh <nijjar.login@gmail.com>
2013-08-22 12:10:12 -0400

    added new about us page

--- assets/libraries/aboutUsLibrary.php
+++ assets/libraries/aboutUsLibrary.php
@@ -1,73 +1,16 @@
 <?php function aboutUSContent() { ?>
-        <div id="pageContent">
-          <div id="aboutUsImageContainer">
-            <div id="aboutUsImage">
-              <div id="progressbar"></div>
+        <div id="pageContent">        
+        	<div id="pageTitle">
+           		<span>ABOUT US</span>
             </div>
-            <div id="aboutUsImageCrossFade"></div>
-          </div>
-          <div id="aboutCarry">
-            <span id="aboutCarryTitle">ABOUT CARRY MATERNITY</span><br><br>
-            <p>
-              Rhonda Maternity, a historic maternity boutique in the heart of Yorkville has transformed into Carry Maternity.At Carry Maternity, we provide the same attentive care and impeccable products that have been bringing pregnant mothers into Rhonda Maternity since 1971.
-            </p>
-          </div><br><br>
-          <div id="featuresSet1">
-             <div id="feature1" class="feature">
-                <span>Nourishing Space</span><br><br>
-                  <p>Carry Maternity offers new and expecting mothers a nourishing space where they can pamper themselves. Our goal is to provide exceptional care to new mothers, so they can better care for themselves and their babies.
-                </p>  
-              </div>  
-              <div id="feature2" class="feature">
-                <span>Affordable Fashion</span><br><br>
-                  <p>At Carry Maternity we know concerns that come with having your first baby. We will help you to find something on a budget. Our mantra is spend well, spend less..
-                </p>
-              </div>  
-          </div>
+            <div id="aboutUsTextContainer">
+            	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eu rhoncus erat. Sed nec gravida magna, a hendrerit
+eros. Fusce eget leo iaculis, suscipit nisi eu, cursus nunc. Curabitur at lacinia justo. Integer ultricies mauris vitae auctor aliquet. Integer tempor interdum leo, eu laoreet tellus tristique ac. Donec vulputate mollis sem eu accumsan. Phasellus accumsan, nunc sit amet tristique gravida, orci magna ullamcorper sem, at ultrices nunc nunc eget velit. Nulla vulputate, nulla eu euismod pharetra, dui nibh scelerisque tortor, et posuere augue sapien at augue. Integer ullamcorper,
+odio eu auctor convallis, diam nisl blandit libero, a blandit eros arcu venenatis magna. Nullam nulla augue, congue vitae velit at, tincidunt aliquam est.</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eu rhoncus erat. Sed nec gravida magna, a hendrerit
+eros. Fusce eget leo iaculis, suscipit nisi eu, cursus nunc. Curabitur at lacinia justo. Integer ultricies mauris vitae auctor aliquet. Integer tempor interdum leo, eu laoreet tellus tristique ac. Donec vulputate mollis sem eu accumsan. Phasellus accumsan, nunc sit amet tristique gravida, orci magna ullamcorper sem, at ultrices nunc nunc eget velit. Nulla vulputate, nulla eu euismod pharetra, dui nibh scelerisque tortor, et posuere augue sapien at augue. Integer ullamcorper,
+odio eu auctor convallis, diam nisl blandit libero, a blandit eros arcu venenatis magna. Nullam nulla augue, congue vitae velit at, tincidunt aliquam est.</p>
+            </div>
           
-          <div id="featuresSet2">
-             <div id="feature3" class="feature">
-                <span>Pregnancy in Style</span><br><br>
-                  <p>Carry Maternity offers a wide-array of original and brand name designs and an exclusive selection of all-natural cosmetics products.
-                </p>  
-              </div>  
-              <div id="feature4" class="feature">
-                <span>Here to Help</span><br><br>
-                  <p>We strive to be a supportive space for new and expecting mothers in Toronto. If you are in the neighbourhood, stop by for one of our workshops, or come in to change a diaper or nurse your child.
-                </p>
-              </div>  
-          </div>
-          <div id="bottomBanner">
-          	<div id="bottomBannerPost1">
-              <div class="meetUsText">
-                <h3>
-                	<br>
-                  MEET<br>
-                  PATRICIA<br>
-                  GILLESPIE<br><br>
-                </h3>
-              </div>
-            </div>
-              <div id="bottomBannerPost2">
-                <div class="meetUsText">
-                  <h3>
-                  	<br>
-                    NEW<br>
-                    STORE<br>
-                    LAUNCH<br><br>
-                  </h3>
-                </div>
-              </div>
-              <div id="bottomBannerPost3">
-                <div class="meetUsText">
-                  <h3>
-                  	<br>
-                    VIEW<br>
-                    OUR<br>
-                    BLOG<br><br>
-                  </h3>
-                </div>
-              </div>
-          </div>
         </div>
 <?php } ?>

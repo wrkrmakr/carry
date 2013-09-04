fec409cb8aa51861d7e813b984d93ab39b0c6c1a
Jeff Quick <delrinbobbins@yahoo.ca>
2013-09-04 16:57:36 -0400

    adding more images

--- assets/libraries/essentialsLibrary.php
+++ assets/libraries/essentialsLibrary.php
@@ -5,27 +5,27 @@
             <div id="productLine1" class="productLine">
             	<div id="product1" class="productCont product1Cont">
                 	<div id="product1Image" class="productImage"></div>
-                    <div class="productName">MAMA MIO</div>  
-                    <div class="productCategory">Lucky Legs</div> 
-                    <div class="productPrize">$ 25</div>               
+                    <div class="productName">CARRY MATERNITY</div>  
+                    <div class="productCategory">Leggings</div> 
+                    <div class="productPrize">$ ??</div>               
                 </div>
                 <div id="product2" class="productCont product2Cont">
                 	<div id="product2Image" class="productImage"></div>
-                    <div class="productName">LULU</div>  
-                    <div class="productCategory">V-NECK</div> 
-                    <div class="productPrize">$ 100</div> 
+                    <div class="productName">??</div>  
+                    <div class="productCategory">Camisole</div> 
+                    <div class="productPrize">$ ??</div> 
                 </div>
                 <div id="product3" class="productCont product3Cont">
                 	<div id="product3Image" class="productImage"></div>
-                    <div class="productName">BELABUMBUM</div>  
-                    <div class="productCategory">TUMMY</div> 
-                    <div class="productPrize">$ 70</div>
+                    <div class="productName">CARRY MATERNITY</div>  
+                    <div class="productCategory">Tube Dress</div> 
+                    <div class="productPrize">$ ??</div>
                 </div>
                 <div id="product4" class="productCont product4Cont">
                 	<div id="product4Image" class="productImage"></div>
-                    <div class="productName">BELLABAND</div>  
-                    <div class="productCategory">BRA</div> 
-                    <div class="productPrize">$ 50</div>
+                    <div class="productName">PAIGE DENIM</div>  
+                    <div class="productCategory">?? Skinny Jeans</div> 
+                    <div class="productPrize">$ ??</div>
                 </div>
             </div>
             <div id="productLine2" class="productLine">
@@ -37,35 +37,35 @@
                 </div>
                 <div id="product6" class="productCont product2Cont">
                 	<div id="product6Image" class="productImage"></div>
-                    <div class="productName">QUEEN MUM</div>  
-                    <div class="productCategory">COCKTAIL</div> 
-                    <div class="productPrize">$ 150</div> 
+                    <div class="productName">CARRY MATERNITY</div>  
+                    <div class="productCategory">PDR Blazer</div> 
+                    <div class="productPrize">$ ??</div> 
                 </div>
                 <div id="product7" class="productCont product3Cont">
                 	<div id="product7Image" class="productImage"></div>
-                    <div class="productName">CARRY METERNITY</div>  
-                    <div class="productCategory">VINTAGE</div> 
-                    <div class="productPrize">$ 90</div> 
+                    <div class="productName">CARRY MATERNITY</div>  
+                    <div class="productCategory">PDR Skirt</div> 
+                    <div class="productPrize">$ ??</div> 
                 </div>
                 <div id="product8" class="productCont product4Cont">
                 	<div id="product8Image" class="productImage"></div>
-                    <div class="productName">NOPPIES</div>  
-                    <div class="productCategory">COCKTAIL 2</div> 
-                    <div class="productPrize">$ 175</div> 
+                    <div class="productName">CARRY MATERNITY</div>  
+                    <div class="productCategory">PDR Pant</div> 
+                    <div class="productPrize">$ ??</div> 
                 </div>
             </div>
             <div id="productLine3" class="productLine">
             	<div id="product9" class="productCont product1Cont">
                 	<div id="product9Image" class="productImage"></div>
-                    <div class="productName">M COAT</div>  
+                    <div class="productName">THE MCOAT</div>  
                     <div class="productCategory">MCoat</div> 
                     <div class="productPrize">$ 420</div>                 
                 </div>
                 <div id="product10" class="productCont product2Cont">
                 	<div id="product10Image" class="productImage"></div>
-                    <div class="productName">QUEEN MUM</div>  
-                    <div class="productCategory">COCKTAIL</div> 
-                    <div class="productPrize">$ 150</div> 
+                    <div class="productName">??</div>  
+                    <div class="productCategory">Maternity Support Band</div> 
+                    <div class="productPrize">$ ??</div> 
                 </div>
                 <div id="product11" class="productCont product3Cont">
                 	<div id="product11Image" class="productImage"></div>

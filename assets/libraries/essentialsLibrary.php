d5ee736694076f2d23f850abc94c84a9680f58f1
Jeff Quick <delrinbobbins@yahoo.ca>
2013-09-04 14:56:05 -0400

    rearranging image files

--- assets/libraries/essentialsLibrary.php
+++ assets/libraries/essentialsLibrary.php
@@ -5,9 +5,9 @@
             <div id="productLine1" class="productLine">
             	<div id="product1" class="productCont product1Cont">
                 	<div id="product1Image" class="productImage"></div>
-                    <div class="productName">CARRY METERNITY</div>  
-                    <div class="productCategory">TOP & BOTTOM</div> 
-                    <div class="productPrize">$ 90</div>               
+                    <div class="productName">MAMA MIO</div>  
+                    <div class="productCategory">Lucky Legs</div> 
+                    <div class="productPrize">$ 25</div>               
                 </div>
                 <div id="product2" class="productCont product2Cont">
                 	<div id="product2Image" class="productImage"></div>
@@ -31,9 +31,9 @@
             <div id="productLine2" class="productLine">
             	<div id="product5" class="productCont product1Cont">
                 	<div id="product5Image" class="productImage"></div>
-                    <div class="productName">ISABELLA OLIVER</div>  
-                    <div class="productCategory">PREGGERS</div> 
-                    <div class="productPrize">$ 100</div>                 
+                    <div class="productName">OCEAN LILY</div>  
+                    <div class="productCategory">Skinny Jean</div> 
+                    <div class="productPrize">$ 110</div>                 
                 </div>
                 <div id="product6" class="productCont product2Cont">
                 	<div id="product6Image" class="productImage"></div>
@@ -52,6 +52,32 @@
                     <div class="productName">NOPPIES</div>  
                     <div class="productCategory">COCKTAIL 2</div> 
                     <div class="productPrize">$ 175</div> 
+                </div>
+            </div>
+            <div id="productLine3" class="productLine">
+            	<div id="product9" class="productCont product1Cont">
+                	<div id="product9Image" class="productImage"></div>
+                    <div class="productName">M COAT</div>  
+                    <div class="productCategory">MCoat</div> 
+                    <div class="productPrize">$ 420</div>                 
+                </div>
+                <div id="product10" class="productCont product2Cont">
+                	<div id="product10Image" class="productImage"></div>
+                    <div class="productName">QUEEN MUM</div>  
+                    <div class="productCategory">COCKTAIL</div> 
+                    <div class="productPrize">$ 150</div> 
+                </div>
+                <div id="product11" class="productCont product3Cont">
+                	<div id="product11Image" class="productImage"></div>
+                    <div class="productName">MAMA MIO</div>  
+                    <div class="productCategory">Tummy Rub Oil</div> 
+                    <div class="productPrize">$ 46</div> 
+                </div>
+                <div id="product12" class="productCont product4Cont">
+                	<div id="product12Image" class="productImage"></div>
+                    <div class="productName">CAKE LINGERIE</div>  
+                    <div class="productCategory">Blueberry Pajama</div> 
+                    <div class="productPrize">$ 115</div> 
                 </div>
             </div>
 <?php } ?>

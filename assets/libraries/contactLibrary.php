53584ac07d189d21e67a8b43f894b9d2ff7e0ea3
Jeff Quick <delrinbobbins@yahoo.ca>
2013-09-17 17:04:16 -0400

    sync

--- assets/libraries/contactLibrary.php
+++ assets/libraries/contactLibrary.php
@@ -7,7 +7,7 @@
                 	<div id="storeInfoContainer">
                     	<div id="storeHours">
                         	<br /><p class="bigContactText">STORE HOURS</p>
-                            <hr class="greyBorder" />
+                            <!--<hr class="greyBorder" />-->
                             <p class="smallContactText"><span style="font-size:16px">OPEN 7 DAYS A WEEK</span><br />
                             	Monday-Wednesday 11am - 6pm<br />
                                 Thursday 11am - 7pm<br />
@@ -16,7 +16,7 @@
                             </p>
                         </div>
                         <div id="locationInfo"><p class="bigContactText">LOCATION</p>
-                            <hr class="greyBorder" />	
+                            <!--<hr class="greyBorder" />-->	
                         	<p class="smallContactText"><span style="font-size:16px">TORONTO</span><br />
                                 110 Cumberland Street<br />
                                 Toronto, ON M5R 1A6<br />
@@ -29,7 +29,7 @@
                 <div id="inquires">	
                 	<div id="inquiresContainer">
                         <br /><p class="bigContactText">INQUIRIES</p>
-                        <hr class="greyBorder" /><br />
+                        <!--<hr class="greyBorder" />--><br />
                         <form id="inquiresForm" action="validateInquiryForm.php" method="post">
                         	<input  class="formTextBox" id="inquiryFormEmail" name="inquiryFormEmail" type="text" placeholder= 			 								"ENTER YOUR E-MAIL" onfocus ="this.placeholder = ''" onblur="this.placeholder= 'ENTER YOUR E-MAIL  								'"  /><br /><br />
                             <textarea id="inquiryFormQuestion" name="inquiryFormQuestion" placeholder="YOUR QUESTION" onfocus= 				 								"this.placeholder = ''" onblur	="this.placeholder= 'YOUR QUESTION'"  ></textarea><br /><br />
